656ec133094922759c91e61ff780479fb620c61d
Murillo  Sousa <murillosm@hotmail.com>
2024-10-13 17:22:58 -0300

    Refatorar código para remover arquivos e trechos de código desnecessários

--- my1stcompiler/run.sh
+++
@@ -1,8 +0,0 @@
-if flex scanner.l ; then
-  if bison -vd parser.y; then
-    if gcc lex.yy.c parser.tab.c sym.c -o compiler -lfl ; then
-      ./compiler < entrada.js
-    fi
-  fi
-fi
-
--- my1stcompiler/sym.h
+++
@@ -1,8 +0,0 @@
-/* (Simple) Symbol Table Definitions */
-#pragma once
-char *stringpool(char *);
-void init_stringpool(int);
-#define NEW(type) (type *) calloc(1,sizeof(type))
-
-
-
